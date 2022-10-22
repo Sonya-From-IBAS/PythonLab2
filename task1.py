f77a1bc34dff897862e3c19fd8618df9ef5a616d
Leonid <Anevskyi2018@yandex.ru>
2022-10-22 22:35:54 +0400

    fixed first task

--- task1.py
+++ task1.py
@@ -2,13 +2,13 @@ import os
 import csv
 
 
-def create_csv_annotation(class_name: str) -> None:
+def create_csv_annotation(class_name: str, annotation_name: str) -> None:
     path_to_class = os.path.join('dataset', class_name)
     class_names = os.listdir(path_to_class)
-    annotation_name = "annotation.csv"
-    with open("annotation.csv", mode="w", encoding="UTF-16", newline='') as file:
+    with open(annotation_name, mode="w", encoding="UTF-16", newline='') as file:
         file_writer = csv.writer(file, delimiter=",")
         for name in class_names:
-            file_writer.writerow([os.path.abspath(name), os.path.join(path_to_class, name), class_name])
+            file_writer.writerow(
+                [os.path.abspath(name), os.path.join(path_to_class, name), class_name])
 
 # first task
